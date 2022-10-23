92c027af2ef55022b72b4f4dbedb83351926429d
Дмитрий Швалев <112343377+DmitryShvalev@users.noreply.github.com>
2022-10-24 04:06:38 +0500

    Add files via upload

---
+++ lab1/ConsoleApplication1/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double n, m;
+   
+    cout << "Введите n\n";
+    cin >> n;
+
+    cout << "Введите m\n";
+    cin >> m;
+    
+    cout << "n++ * m = " << (n++ * m);
+
+    cout << "\nn++ < m = " << (n++ < m);
+    
+    cout << "\nm-- > m = " << (m-- > m);
+
+    return 0;
+}
